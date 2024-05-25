049abe4cd014832100e991edd8d0400b35cbc2f6
Abubacer <azizdesignislife@gmail.com>
2024-05-25 19:45:42 +0000

    Add react-unicons for icon support - Installed iconscout/react-unicons to provide a comprehensive set of icons for the application UI. - Updated package.json and package-lock.json with the new dependency. - Tested the new icons.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { UilReact } from '@iconscout/react-unicons';
 
 function App() {
   return (
     <div className="App">
       <h1 className='font-bold text-xl text-blue-500'>Hello World!</h1>
+      <UilReact sizesize={48} className="items-center" />
     </div>
   );
 }
