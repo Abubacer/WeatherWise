8174cdc25aa4a1c090d8aa12187a20a21381f38a
Abubacer <azizdesignislife@gmail.com>
2024-05-27 01:56:46 +0000

    Add the geolocalisation button to the header component - Import the geolocalisation icon from react-unicons. - Applied tailwind css  styling to maintain a cohesive look and feel.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { UilMapMarker } from '@iconscout/react-unicons'
 
 const Header = () => {
     return (
         <div className="bg-white p-4 flex items-center">
             <h1 className="text-lg text-gray-400 cursor-pointer hover:text-gray-400 transition ease-out mr-8">WeatherWise</h1>
+            <button className="p-2 border border-gray-300 bg-gray-50 text-gray-800 rounded-3xl mr-2 hover:border-blue-400 hover:bg-blue-400 transition ease-out">
+                <UilMapMarker size={22} className="text-gray-400 cursor-pointer hover:text-white" />
+            </button>
         </div >
     );
 };
