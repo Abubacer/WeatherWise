f1f2445d7b4ac15f4b91c7fc4d3400f0cc09aa86
Abubacer <azizdesignislife@gmail.com>
2024-05-27 01:54:53 +0000

    Add the app logo to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="bg-white p-4 flex items-center">
+            <h1 className="text-lg text-gray-400 cursor-pointer hover:text-gray-400 transition ease-out mr-8">WeatherWise</h1>
         </div >
     );
 };
