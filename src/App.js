a5ca5de7a8d5eb0fb5f30045d5444315107c2059
Abubacer <azizdesignislife@gmail.com>
2024-05-27 02:15:26 +0000

    Implement the main content UI component - Add the component to the app.js. - Create the component js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div className="h-screen">
       <Header />
+      <div className="">
+        <MainContent />
+      </div>
     </div>
   );
 }
