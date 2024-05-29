dbe1a68a4f84a1546e411ee1f205f5ed17abee7b
Abubacer <azizdesignislife@gmail.com>
2024-05-29 05:17:12 +0000

    Implemented the weather icon mapping for my custom weather icons - Removed the rounded edge from Header component div - Created a new file for the icon mapping. - Exported the iconMapping to use in my app. - Uploaded and stored my custom icons in src/weathericons/.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import WelcomeMessage from './components/WelcomeMessage'
-import fetchFormatedWeatherData from './apis/weatherdata';
+//import fetchFormatedWeatherData from './apis/weatherdata';
 
 function App() {
 
-  const getWeather = async () => {
+  /*const getWeather = async () => {
     const weatherData = await fetchFormatedWeatherData({ q: "tokyo" });
     console.log(weatherData);
   };
 
-  getWeather();
+  getWeather();*/
 
   return (
     <div className="h-screen bg-gradient-to-br from-blue-200 to-orange-100">
       <div className="backdrop-blur-sm bg-sky-800/10 h-screen">
-        <div className="p-4 rounded-3xl relative z-30">
+        <div className="p-4 relative z-30">
           <Header />
         </div>
         <div className="relative z-20">
---
+++ src/weatherIconMapping.js
@@ -0,0 +1,22 @@
+const iconMapping = {
+    '01d': '/src/weathericons/01d.svg',
+    '01n': '/src/weathericons/01n.svg',
+    '02d': '/src/weathericons/02d.svg',
+    '02n': '/src/weathericons/02n.svg',
+    '03d': '/src/weathericons/03d.svg',
+    '03n': '/src/weathericons/03n.svg',
+    '04d': '/src/weathericons/04d.svg',
+    '04n': '/src/weathericons/04n.svg',
+    '09d': '/src/weathericons/09d.svg',
+    '09n': '/src/weathericons/09n.svg',
+    '10d': '/src/weathericons/10d.svg',
+    '10n': '/src/weathericons/10n.svg',
+    '11d': '/src/weathericons/11d.svg',
+    '11n': '/src/weathericons/11n.svg',
+    '13d': '/src/weathericons/13d.svg',
+    '13n': '/src/weathericons/13n.svg',
+    '50d': '/src/weathericons/50d.svg',
+    '50n': '/src/weathericons/50n.svg',
+};
+
+export default iconMapping;
